683ba36d8a724c6825e8e8ddf48d6d99b295c8f2
Kevin Solkowski <ksolkowski@gmail.com>
2018-02-23 18:19:40 -0600

    ok. i think this is good now?

--- app.rb
+++ app.rb
@@ -2,38 +2,46 @@
 require "roda"
 require 'open-uri'
 require 'nokogiri'
-
 require "redis"
 require "json"
+require "base64"
 
 uri = URI.parse(ENV["REDIS_URL"]||"redis://localhost:6379")
 $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+ENV["SITE_URL"] ||= "localhost:3000"
 
 class CatApi < Roda
   EXPIRES_IN = ((60 * 5) * 100) # 5 min in ms
   EXPIRE_KEY = "cats:expire:"
   STORE_KEY  = "cats:urls:"
+  STORED_IMAGE_KEY = "cats:images:"
   NO_CAT_LIST = ['austinkahly', 'murph', 'nichelle']
+  path = File.expand_path '../', __FILE__
+  PATH = path
 
   plugin :json
 
   def fetch_or_download_cat_urls
     if cat_urls = $redis.get(STORE_KEY) # cats exist
-      clear_cat_urls if urls_expired?
-      JSON.parse(cat_urls).sample
+      clear_cached_cats if urls_expired?
+      url = JSON.parse(cat_urls).sample
     else
       page = (0..2010).to_a.sample
       html = Nokogiri::HTML(open("http://d.hatena.ne.jp/fubirai/?of=#{page}"))
       cat_urls = html.css("img.hatena-fotolife").to_a.map{|child| child.attributes["src"].value }
       store_cat_urls(cat_urls)
-      cat_urls.sample
+      url = cat_urls.sample
     end
+
+    #url = "https://cdn-ak.f.st-hatena.com/images/fotolife/f/fubirai/20080521/20080521234056.jpg"
+
+    decoded_image, path = save_image_to_redis(url)
   end
 
   def urls_expired?
-    cats_set = $redis.get EXPIRE_KEY
-    return false if cats_set.nil?
-    cats_set.to_i < Time.now.to_i
+    cats_expires = $redis.get EXPIRE_KEY
+    return false if cats_expires.nil?
+    cats_expires.to_i < Time.now.to_i
   end
 
   def store_cat_urls(urls)
@@ -47,61 +55,49 @@ class CatApi < Roda
   end
 
   # some helper functions for images
-  def url_to_filename(url)
+  def url_to_redis_key(url)
     # pop off the tail png
-    clean_url = url.gsub(".png", "")
-    clean_url = clean_url.gsub(".jpg", "")
-    clean_url.gsub!(/[^0-9A-Za-z\-]/, '_')
-
-    # store these in the tmp folder!
-    # always png!
-    Rails.root.join('tmp', "#{clean_url}.png")
+    key = url.gsub(/[^0-9A-Za-z\-]/, '_').gsub("jpg", "")# + ".jpg"
+    STORED_IMAGE_KEY + key
   end
 
-  def open_or_save_image(url)
-    path = url_to_filename(url)
-    if image_saved_on_file?(path) #and !@single # single ones don't care about
-      @open_images[path]
-    else
-      @downloaded_images_count ||= 0
-      @downloaded_images_count += 1
-      save_to_tempfile(url, path)
+  def already_saved?(key)
+    $redis.exists(key)
+  end
+
+  def fetch_saved_image(key)
+    $redis.get(key)
+  end
+
+  def save_image(url, key)
+    raw_img = Base64.encode64(open(url).read)
+    $redis.set key, raw_img
+    raw_img
+  end
+
+  def fetch_and_decode(key)
+    raw_img = fetch_saved_image(key)
+    decode_image(raw_img)
+  end
+
+  def decode_image(raw_img)
+    Base64.decode64 raw_img
+  end
+
+  def key_to_url(key)
+    key.gsub(STORED_IMAGE_KEY, '') + ".jpg"
+  end
+
+  def save_image_to_redis(url)
+    key = url_to_redis_key(url)
+
+    if already_saved?(key)
+      raw_img = fetch_saved_image(key)
+    else # store it in redis
+      raw_img = save_image(url, key)
     end
-    path
-  end
 
-  def image_saved_on_file?(path)
-    path = Rails.root.join('tmp', path)
-    @open_images ||= {}
-    @open_images[path] = path if File.exists?(path) # add it to the open images hash
-    File.size?(path)
-  end
-
-  def save_to_tempfile(url, path)
-    @open_images ||= {}
-    @open_images[path] = path
-    if !File.size?(path)
-      File.open(path, 'wb+') do |file|
-        file.binmode
-
-        retries = 0
-        begin
-          downloaded_file = open(url).read
-          file.write downloaded_file
-        rescue => e
-          if retries < 5
-            retries += 1
-            Rails.logger.info "retrying to download #{url} with #{e.message} failed #{retries} times so far"
-            retry
-          else
-            raise
-          end
-        end
-      end
-      path
-    else
-      path
-    end
+    [decode_image(raw_img), key_to_url(key)]
   end
 
   route do |r|
@@ -112,29 +108,33 @@ class CatApi < Roda
 
     r.on "cats" do
       if r.is_get?
-        "<img src=\"#{fetch_or_download_cat_urls}\"></img>"
+        response['Content-Type'] = "text/plain"
+        decoded_image, fake_path = fetch_or_download_cat_urls
+        decoded_image
       else
         response['Content-Type'] = 'application/json'
         if NO_CAT_LIST.include?(r.params["user_name"]) and r.params["text"] != "cats are great"
           {
-            "response_type": "in_channel",
-            "text": "Come back when you have a cat"
+            response_type: "in_channel",
+            text: "Come back when you have a cat"
           }
         else
-          image = fetch_or_download_cat_urls
+          decoded_image, fake_path = fetch_or_download_cat_urls
+
+          real_url = File.join ENV["SITE_URL"], fake_path
           clear_cached_cats if r.params["text"] != "clear"
           {
-            "response_type": "in_channel",
-            "attachments": [
+            response_type: "in_channel",
+            attachments: [
               {
-                  "fallback": "<3 Cats <3",
-                  "color": "#36a64f",
-                  "title": "Check out this cat",
-                  "title_link": "Cats",
-                  "fields": [],
-                  "image_url": image,
-                  "thumb_url": image,
-                  "ts": Time.now.to_i
+                fallback: "<3 Cats <3",
+                color: "#36a64f",
+                title: "Check out this cat",
+                title_link: "Cats",
+                fields: [],
+                image_url: real_url,
+                thumb_url: real_url,
+                ts: Time.now.to_i
               }
             ]
           }.to_json
@@ -146,5 +146,16 @@ class CatApi < Roda
       clear_cached_cats
       "cleared"
     end
+
+
+    r.get do
+      cleaned = request.remaining_path[1..-1].gsub(".jpg", "")
+      key = url_to_redis_key(cleaned)
+      if already_saved?(key)
+        response['Content-Type'] = "text/plain"
+        fetch_and_decode(key)
+      end
+    end
+
   end
 end
