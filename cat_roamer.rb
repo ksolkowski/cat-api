da027feb0636f7a925a469561f221b9676c1e85b
Kevin Solkowski <ksolkowski@gmail.com>
2018-02-28 21:25:40 -0600

    sort it!

--- cat_roamer.rb
+++ cat_roamer.rb
@@ -61,7 +61,7 @@ module CatRoamer
     action_button = payload['actions'].first # what button was pressed
     original_attachment = original_message['attachments'].find{|x| x["callback_id"] == callback_id }
     user = payload["user"]
-    actions = original_attachment["actions"].map do |btn|
+    actions = original_attachment["actions"].sort_by{|btn| btn["value"] == action_button["value"] }.map do |btn|
       vote_key = btn["value"] == AWW ? AWW : DAWWW
 
       if btn["value"] == action_button["value"] # this is the action
