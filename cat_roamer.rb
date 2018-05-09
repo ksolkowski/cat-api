5dca2405b92f45bed26e46aabad536c29c723097
Kevin Solkowski <ksolkowski@gmail.com>
2018-05-09 10:01:11 -0500

    I got abs

--- cat_roamer.rb
+++ cat_roamer.rb
@@ -67,6 +67,7 @@ end
   def combine_some_cats(count=6)
     count = 30 if count >= 30
     count = 2 if count == 1
+    count = count.abs
     size = Image.group_and_count(:width, :height).all.select{|x| x[:count] > 100 }.sample
     images = Image.random(count).where{width =~ size.width}.where{height =~ size.height}.all
     joined_ids = images.map(&:id).join("_")
