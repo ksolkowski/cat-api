2ee85bc272a427855254c231584a675f0cb042a5
Kevin Solkowski <ksolkowski@gmail.com>
2018-08-27 22:13:19 -0500

    maybe thjis?

--- routes/pokemon.rb
+++ routes/pokemon.rb
@@ -7,10 +7,10 @@ class CatApi < Roda
     available = pokemans.map{|x| x["Pokemon Attracted"] }.flatten.map{|x| x.split(" - ").first }.
                 inject(Hash.new(0)){|h, p| h[p]+=1;h }.sort_by{|p, c| sorty[p].to_i }
 
-    r.get ":name", String do |name|
-      @recipies = pokemans.select{|x| x["Pokemon Attracted"].any?{|z| z.parameterize.include?(name)} }.map do |recipie|
+    def find_recipies(name, pokemans)
+      pokemans.select{|x| x["Pokemon Attracted"].any?{|z| z.include?(name)} }.map do |recipie|
         r = recipie.dup
-        r["Percent"] = recipie["Pokemon Attracted"].find{|z| z.parameterize.include?(name) }.split("-").last
+        r["Percent"] = recipie["Pokemon Attracted"].find{|z| z.include?(name) }.split("-").last
         r.delete("Pokemon Attracted")
         r
       end.sort_by do |r|
@@ -27,13 +27,13 @@ class CatApi < Roda
           4
         end
       end
-
-      @header = @recipies.first.keys
-      view("pokemon/recipie")
     end
 
     r.get do
-      @pokemon = available
+      @pokemon = available.map do |name, count|
+        {"name" => name, "count" => count, "details" => find_recipies(name, pokemans)}
+      end
+
       view("pokemon/index")
     end
 
