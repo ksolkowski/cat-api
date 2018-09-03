3339fb195f7b17052b941d02c50bd7d96824f081
Kevin Solkowski <ksolkowski@gmail.com>
2018-09-03 12:34:32 -0500

    ok.

--- routes/pokemon.rb
+++ routes/pokemon.rb
@@ -1,25 +1,34 @@
-require "yaml"
+require 'yaml'
+require 'ostruct'
 
 class CatApi < Roda
-  route('pokemon') do |r|
-    pokemans = YAML.load_file("./config/pokemon/recipies.yml")
-    sorty = YAML.load_file("./config/pokemon/sort_order.yml")
-    available = pokemans.map{|x| x["Pokemon"] }.flatten.map{|x| x.split(" - ").first }.
-                inject(Hash.new(0)){|h, p| h[p]+=1;h }.sort_by{|p, c| sorty[p].to_i }
+  plugin :path
+  path   :pokemon, '/pokemon'
+  path   :recipies, '/pokemon/recipies'
+  path   :recipie do |recipie|
+    "/pokemon/recipies/#{recipie}"
+  end
 
-    all_recipies = YAML.load_file("./config/pokemon/recipies.yml")
-    @header = all_recipies.map{|x| x["Name"] }.uniq.map do |recipie|
-      { name: recipie.humanize, url: "/pokemon/recipies/#{recipie.parameterize}" }
+  route('pokemon') do |r|
+    ALL_RECIPIES = YAML.load_file("./config/pokemon/recipies.yml").map{|hash| OpenStruct.new(hash.inject({}){|h,(k,v)| h[k.downcase]=v;h }.freeze) }
+    sorty = YAML.load_file("./config/pokemon/sort_order.yml").map{|hash| OpenStruct.new(hash.inject({}){|h,(k,v)| h[k.downcase]=v;h }.freeze) }
+    available = ALL_RECIPIES.map(&:pokemon).flatten.map{|x| x.split(" - ").first }.
+                inject(Hash.new(0)){|h, p| h[p]+=1;h }.sort_by{|name, count| sorty.find{|x| x.name == name }.number.to_i }
+
+
+    @header = ALL_RECIPIES.map(&:name).uniq.map do |recipie|
+      url = recipie_path(recipie.parameterize)
+      { name: recipie.humanize, url: url, active: r.path == url }
     end
 
-    def find_recipies(name, pokemans)
-      pokemans.select{|x| x["Pokemon"].any?{|z| z.include?(name)} }.map do |recipie|
-        r = recipie.dup
-        r["Percent"] = recipie["Pokemon"].find{|z| z.include?(name) }.split("-").last.strip
-        r.delete("Pokemon")
-        r
+    def find_recipies(name)
+      ALL_RECIPIES.select{|x| x.pokemon.any?{|z| z.include?(name)} }.map do |recipie|
+        ingredients = recipie.ingredients
+        percent = recipie.pokemon.find{|z| z.include?(name) }.split("-").last.strip
+        rarity = recipie.rarity
+        OpenStruct.new(rarity: rarity, ingredients: ingredients, percent: percent, name: recipie.name, attracts: recipie.attracts)
       end.sort_by do |r|
-        rarity = r["Rarity"]
+        rarity = r.rarity
         if rarity == "Basic"
           0
         elsif rarity == "Good" or rarity.start_with?("Good")
@@ -36,18 +45,18 @@ class CatApi < Roda
 
     r.on "recipies" do
       r.on(:name) do
-        @recipies = all_recipies.select{|x| x["Name"].parameterize == r.params["name"] }
+        @recipies = ALL_RECIPIES.select{|x| x.name.parameterize == r.params["name"] }
         view("pokemon/recipie")
       end
 
       r.is do
         r.get do
-          @recipies = all_recipies.map do |recipie|
-            name = recipie["Name"]
+          @recipies = ALL_RECIPIES.map do |recipie|
+            name = recipie.name
             {
               "name" => name,
-              "attracts" => recipie["Attracts"],
-              "url" => "/pokemon/recipies/#{name.parameterize}"
+              "attracts" => recipie.attracts,
+              "url" => recipie_path(name.parameterize)
             }
           end.uniq
           view("pokemon/recipies")
@@ -57,8 +66,8 @@ class CatApi < Roda
 
     r.get do
       @pokemon = available.map do |name, count|
-        details = find_recipies(name, pokemans)
-        max_percent = details.max_by{|x| x["Percent"].gsub("%", "").to_f }
+        details = find_recipies(name)
+        max_percent = details.max_by{|x| x.percent.gsub("%", "").to_f }.percent
         {"name" => name, "count" => count, "details" => details, "max_percent" => max_percent}
       end
 
